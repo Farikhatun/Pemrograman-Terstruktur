01073d493abc9d786f440d200f4187e750e8526c
Farikhatun <89762932+Farikhatun@users.noreply.github.com>
2021-11-29 15:30:49 +0700

    Delete Project01.py

--- Chapter09/Project01.py
+++
@@ -1,9 +0,0 @@
-def ubahHuruf(teks, a, b):
-    listTeks = list(teks)
-    for char in listTeks:
-        if(char == a):
-            char = b
-    return teks.replace(a, b)
-
-print(ubahHuruf('MATEMATIKA','T','S'))
-
