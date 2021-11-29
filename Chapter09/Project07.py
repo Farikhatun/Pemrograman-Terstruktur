22fc6270b95b24a5f056007b33ba2011afc94719
Farikhatun <89762932+Farikhatun@users.noreply.github.com>
2021-11-29 15:29:37 +0700

    Update Project07.py

--- Chapter09/Project07.py
+++ Chapter09/Project07.py
@@ -3,7 +3,8 @@ mhs = ['K001:ROSIHAN ARI:1979-09-01:SOLO',
        'K003:FAZA FAUZAN:2005-01-28:KARANGANYAR']
 
 print('='*65)
-print('NIM'.ljust(9), 'NAMA MAHASISWA'.ljust(25), 'TGL LAHIR'.ljust(14), 'TEMPAT LAHIR'.ljust(14))
+print('NIM'.ljust(9), 'NAMA MAHASISWA'.ljust(25),
+      'TGL LAHIR'.ljust(14), 'TEMPAT LAHIR'.ljust(14))
 print('-'*65)
 
 for i in mhs:
@@ -11,11 +12,13 @@ for i in mhs:
     nim      = dataList[0]
     nama     = dataList[1]
     tglLahir = dataList[2]
-    dataTglLahir      = tglLahir.split('-')
-    newFormatTglLahir = '{0}/{1}/{2}'.format(dataTglLahir[2], dataTglLahir[1], dataTglLahir[0])
-    tempatLahir       = dataList[3]
+    TglLahir2   = tglLahir.split('-')
+    TglLahir3   = '{0}/{1}/{2}'.format(TglLahir2[2], TglLahir2[1], TglLahir2[0])
+    tempatLahir = dataList[3]
     
-    print(nim.ljust(9), nama.ljust(25), newFormatTglLahir.ljust(14), tempatLahir.ljust(14))
+    print(nim.ljust(10), end='')
+    print(nama.ljust(26), end='')
+    print(TglLahir3.ljust(15), end='')
+    print(tempatLahir.ljust(14))
 
 print('-'*65)
-
