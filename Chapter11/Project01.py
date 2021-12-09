4de6bf22263e6e1410bd1b23d4e46472c13ff194
Farikhatun <89762932+Farikhatun@users.noreply.github.com>
2021-12-09 23:17:54 +0700

    Update Project01.py

--- Chapter11/Project01.py
+++ Chapter11/Project01.py
@@ -3,6 +3,7 @@ from datetime import *
 def diffDate(x):
     sekarang = datetime.date(datetime.now())
     x = datetime.strptime(x, '%Y-%m-%d').date()
-    return abs(x-sekarang)
+    return (sekarang-x)
 
-print('selisih harinya adalah {0} hari'.format(diffDate('2021-11-09').days))
+y = diffDate('2018-12-30')
+print('selisih {0} hari'.format(y.days))
