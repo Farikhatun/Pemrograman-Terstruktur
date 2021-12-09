90bb61404da55a4c9e2397e8c1fcc3740d439d07
Farikhatun <89762932+Farikhatun@users.noreply.github.com>
2021-12-09 23:29:11 +0700

    Update Project02.py

--- Chapter11/Project02.py
+++ Chapter11/Project02.py
@@ -1,22 +1,22 @@
-file = open('E:\Peminjaman.txt', 'a')
+file = open('E:\Peminjaman2.txt', 'a')
 
 from datetime import*
 
 def pengembalian(now):
     return now + timedelta(days=7)
 
-lagi = 'y' or 'Y'
 teks = ''
-
-while lagi == 'y':
+while True:
     kode = input('Masukkan Kode Member   : ')
     nama = input('Masukkan Nama Member   : ')
     judul = input('Masukkan Judul Buku    : ')
 
     now = datetime.date(datetime.now())
     teks += '{0}|{1}|{2}|{3}|{4}|\n'.format(kode,nama,judul,now,pengembalian(now))
-    lagi = input('\nUlangi lagi (y/n)? ')
+    ans = input('\nUlangi lagi (y/n)? ')
     print()
+    if ans in ('N', 'n'):
+        break
 
 file.write(teks)
 file.close()
